4d814412a4c19f443e8777b51fe4cf3cc774dbc3
luyanci <2058282593@qq.com>
2024-07-15 15:59:18 +0800

    ignore

--- lib/ignore.py
+++ lib/ignore.py
@@ -2,5 +2,6 @@ def check_ban_inital(uid:str):
     #黑名单，遇到以下uid直接忽略
     if uid == '3546377875360540':
         return True
-
+    if uid == '3546632014531086':
+        return True
     return False
